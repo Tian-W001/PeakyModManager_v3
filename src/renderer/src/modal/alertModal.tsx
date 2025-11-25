3d7bf0dc57b9130904a8e99b773aaf98bc582d08
Tian-W001 <62647645+Tian-W001@users.noreply.github.com>
2025-11-26 04:13:54 +1100

    Prevent drag selection and italicize alert modal actions
    Added user-drag CSS properties to :root to prevent drag selection globally. Updated alertModal action buttons to use italic style for improved visual distinction.

--- src/renderer/src/modal/alertModal.tsx
+++ src/renderer/src/modal/alertModal.tsx
@@ -16,12 +16,10 @@ const AlertModal: React.FC<AlertModalProps> = ({ title, message, actions }) => {
     <div className="fixed inset-0 z-50 flex flex-col items-center justify-center border-2 bg-black/80">
       <div className="relative h-[3px] w-full bg-white/20" />
       <div className="relative z-5 flex h-[30%] w-full flex-col items-center justify-center gap-4 border-y-4 border-y-black bg-[url('@renderer/assets/zzz_site_background.webp')] bg-cover">
-        {/* Message */}
         <h1 className="text-3xl font-extrabold text-white">{title}</h1>
         {message && <p className="mt-2 font-bold text-[#aaa]">{message}</p>}
 
-        {/* Buttons */}
-        <div className="absolute bottom-0 left-1/2 flex -translate-x-1/2 translate-y-[calc(50%+4px)] gap-4">
+        <div className="absolute bottom-0 left-1/2 flex -translate-x-1/2 translate-y-[calc(50%+4px)] gap-4 *:italic">
           {actions.map((action, index) => (
             <button key={index} onClick={action.f} className="w-50">
               {action.name}
