b848a94834e5548b79cede120bf14da1a3590fad
Tian-W001 <62647645+Tian-W001@users.noreply.github.com>
2025-12-11 23:21:46 +1100

    fix alert modal background image
    Changed the background image in AlertModal from zzz_site_background.webp to wallpapers/zzz_wallpaper_0.jpg for improved visual appearance.

--- src/renderer/src/modal/alertModal.tsx
+++ src/renderer/src/modal/alertModal.tsx
@@ -15,7 +15,7 @@ const AlertModal: React.FC<AlertModalProps> = ({ title, message, actions }) => {
   return (
     <div className="modal-overlay" id="modal-overlay">
       <div className="relative h-[3px] w-full bg-white/20" />
-      <div className="relative z-5 flex h-[30%] w-full flex-col items-center justify-center gap-4 border-y-4 border-y-black bg-[url('@renderer/assets/zzz_site_background.webp')] bg-cover">
+      <div className="relative z-5 flex h-[30%] w-full flex-col items-center justify-center gap-4 border-y-4 border-y-black bg-[url('@renderer/assets/wallpapers/zzz_wallpaper_0.jpg')] bg-cover">
         <h1 className="text-3xl font-extrabold text-white">{title}</h1>
         {message && <p className="mt-2 font-bold text-[#aaa]">{message}</p>}
 
